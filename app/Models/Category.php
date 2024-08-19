25bb2d6b50fcee1fecf0ea9786c6633a78f6ecb4
LUCAS FEITOSA ALMEIDA ROCHA <0031432412020@lti.fatecso.edu.br>
2024-08-19 18:05:06 -0300

    Models de Post, Tópico, Tag, Categoria e Comentário

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Topic extends Post
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'title',
         'description',
-        'status',
     ];
 
-    public function category()
+    public function topics()
     {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function tags(){
-        return $this->belongsToMany(Tag::class);
+        return $this->hasMany(Topic::class);
     }
 }
