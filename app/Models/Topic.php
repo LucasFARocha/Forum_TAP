f3b2245cbf001860c83823fcab1cd195828882d6
LUCAS FEITOSA ALMEIDA ROCHA <0031432412020@lti.fatecso.edu.br>
2024-08-28 16:26:27 -0300

    Criação das migrations

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -19,8 +19,11 @@ class Topic extends Post
     {
         return $this->belongsTo(Category::class);
     }
-
     public function tags(){
         return $this->belongsToMany(Tag::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
