1550621ebcc46e5f718eb7e169d2ea6de70d2668
LUCAS FEITOSA ALMEIDA ROCHA <0031432412020@lti.fatecso.edu.br>
2024-08-28 15:46:49 -0300

    Criado modelo Rate para avaliação

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
         'photo',
+        'role',
     ];
 
     /**
@@ -47,8 +48,17 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function rates()
+    {
+        return $this->hasMany(Rate::class);
+    }
 
-    // public function rates(){
-    //     return $this->belongsToMany(Rate::class);
-    // }
+    public function isModerator()
+    {
+        return $this->role === 'moderator' || $this->role === 'admin';
+    }
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
